3665d76a0c9d4da66af3e52f0748c28884a65b53
Pete Boucher <petebouch@gmail.com>
2014-01-13 16:51:50 +0100

    Donation model

---
+++ application/models/donation_model.php
@@ -0,0 +1,68 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Donation_model extends CI_Model {
+
+	var $donationID = '';
+	var $donorID = '';
+	var $paymentID = '';
+	var $goalID = '';
+	var $ammout = '';
+	var $currency = '';
+	var $dateCreated = '';
+	var $dateDisbursed = ''; // date of payment to project organise
+
+	privat vat $is_saved = FALSE;
+
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	public function new($donor, $goal, $ammout, $currency)
+	{
+		$this->donorID = $donor;
+		$this->goalID = $goal;
+		$this->ammout = $ammout;
+		$this->currency = $currency;
+
+		return $this;
+	}
+
+	public function get($id = FALSE)
+	{
+		if ($id){
+			//TODO make the result an instance of this model
+			return = $this->db->get_where('donation',
+				array('DonationID' => , $id));
+		} else {
+			//TODO make the result an array os instances of this model
+			return = $this->db->get('donation');
+		}
+	}
+
+	function save()
+	{
+		if ($this->is_unsaved){
+			if ($this->db->insert('Donation', $this)) {
+				$this->is_saved = TRUE;
+				$this->donationID = $this->db->insert_id();
+			} else {
+				//TODO sad path
+			}
+		} else {
+			if ($this->db->update('Donation', $this, "id = $this->donationID")) {
+				$this->is_saved = TRUE;
+			} else {
+				//TODO sad path
+			}
+		}
+	}
+
+	public function	get_by_goal(){}
+
+	public function get_by_donor(){}
+
+
+
+}
+	/* End of file donation_model.php */
+	/* Location: ./application/models/donation_model.php */
